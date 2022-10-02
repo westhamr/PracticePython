72adc6b7fe4f9817945921829497f1ff784caf91
westhamr <westhamr@yandex.ru>
2022-10-02 22:30:01 +0700

    Change Task 11

--- Task11.py
+++ Task11.py
@@ -5,9 +5,3 @@ list = []
 for i in range(1, num+1):
     list.append(round((1+1/i)**i, 3))
 print(round(sum(list), 3))
-
-
-
-# n = int(input())
-# lst = [round((1+1/i)**i, 3) for i in range(1, n+1)]
-# print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 3)}')
